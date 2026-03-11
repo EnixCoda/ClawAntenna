fcde3314ddc5c8434ed81669da84f1725c355a64
EnixCoda <enixcoda@gmail.com>
2026-03-11 12:56:59 +0800

    Initial Commit

---
+++ Porter/PorterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PorterApp.swift
+//  Porter
+//
+//  Created by Yang, Yixin on 2026/3/11.
+//
+
+import SwiftUI
+
+@main
+struct PorterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
